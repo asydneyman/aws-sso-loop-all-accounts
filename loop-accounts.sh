be38d97984fafde78edf238bd2922efe30f28129
Mahesh Bisl <bisl.mahesh@gmail.com>
2021-05-13 08:32:36 +1000

    Update loop-accounts.sh

--- loop-accounts.sh
+++ loop-accounts.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+set -x
+
+pip install --user boto3
+
+aws configure sso
+
+# Create List of AWS accounts
+# And
+# sso profiles
+mv ~/.aws/config ~/.aws/config.backup
+rm accounts.txt
+python3 make-account-profiles.py bhp
+
+# Enable Automatic Key roation and save the status
+rm -fr enable-kms-cmks-key-rotation*.csv
+
 for account in $(< accounts.txt)
 do
     profile=sso-$account
-    python3 enable-kms-cmks-key-rotation.py $profile >> enable-kms-cmks-key-rotation-status.csv
+    python3 enable-kms-cmks-key-rotation.py $profile
 done
